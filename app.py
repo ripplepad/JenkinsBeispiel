5a08036f771aebc5b3c39b5f876e979f5d837e86
ripplepad <kerne-prosaisch.4l@icloud.com>
2024-12-20 00:45:56 +0100

    o

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 def hello_world():
     return "Hello, World!"
 
+def greet_person(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     print(hello_world())
+    print(greet_person("Alice"))
 
